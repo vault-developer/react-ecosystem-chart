e3582a5b162626ed295a5aba671eb386ca0e0090
Albert Trott <trott.alik@gmail.com>
2024-09-22 13:09:34 +0200

    feat: add UI kits and data fetching

--- charts.data.js
+++ charts.data.js
@@ -5,6 +5,7 @@ export const charts = [
             {name: 'React.js', repo: 'facebook/react', reactOnly: true},
             {name: 'Next.js', repo: 'vercel/next.js', reactOnly: true},
             {name: 'Gatsby.js', repo: 'gatsbyjs/gatsby', reactOnly: true},
+            {name: 'Remix', repo: 'remix-run/remix', reactOnly: true},
             {name: 'Redwood', repo: 'redwoodjs/redwood', reactOnly: true},
         ]
     },
@@ -24,6 +25,24 @@ export const charts = [
             {name: 'Styled-components', repo: 'styled-components/styled-components', reactOnly: true},
             {name: 'Emotion', repo: 'emotion-js/emotion', reactOnly: true},
             {name: 'Unocss', repo: 'unocss/unocss', reactOnly: false},
+        ]
+    },
+    {
+        category: 'UI kit & components',
+        items: [
+            {name: 'Material-ui', repo: 'mui/material-ui', reactOnly: true},
+            {name: 'Ant-design', repo: 'ant-design/ant-design', reactOnly: true},
+            {name: 'Chakra-ui', repo: 'chakra-ui/chakra-ui', reactOnly: true},
+            {name: 'Daisy-ui', repo: 'saadeghi/daisyui', reactOnly: false},
+        ]
+    },
+    {
+        category: 'Data fetching',
+        items: [
+            {name: 'Axios', repo: 'axios/axios', reactOnly: false},
+            {name: 'TanStack', repo: 'TanStack/query', reactOnly: true},
+            {name: 'Swr', repo: 'vercel/swr', reactOnly: true},
+            {name: 'Apollo-client', repo: 'apollographql/apollo-client', reactOnly: false},
         ]
     },
 ];
